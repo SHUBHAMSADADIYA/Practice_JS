84ed2ae6fcfc1a225ce9af31a1007ad226209e98
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-11-30 18:13:34 +0530

    03_Conversions

---
+++ Basics/03_Conversons&Operation.js
@@ -0,0 +1,37 @@
+let score = "Shubham";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+// Both are used
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// Output with score(Different_values)
+// Value   => Output
+
+// "33"    => 33
+// "33abc" => NaN(Not a Number)
+// true    => 1
+// false   => 0
+
+let isLoggedIn = "Shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+console.log(isLoggedIn);
+
+// It returns true/false
+// 1         => true
+// 0         => false
+// ""        => false
+// "Shubham" => true
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
