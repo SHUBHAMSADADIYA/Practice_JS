722b166fbc79decf473f8db8ffb69c931ed9431f
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-11-30 17:31:36 +0530

    02_DataTypes

---
+++ Basics/02_DataTypes.js
@@ -0,0 +1,34 @@
+"use strict" // Treat full code of JS as newer virsion
+
+// alert(3 + 3)
+// we are using Nodejs , not using Browser to run the code
+
+console.log(3 
+    +
+     3);
+// We should not write code like above
+// We should make our code Readable like below
+
+console.log(3 + 3);
+
+// There are main 3 Types of Datatype
+
+/*
+    1. Number  => It has range, number could be entered under 2's power 53 = 9007199254740992.
+    2. String  => String written between "" (Double Quote).
+    3. Boolean => It returns True/False.
+*/
+
+// There are 3 values that are most shown in your code
+
+/*
+    1. NULL      => It is Standalone value which shows empty value,
+                    but Type of NULL is Object
+    2. UNDEFINED => It Defines the value is Undefined.
+    3. Symbol    => It is used to find Uniqueness.
+*/ 
+
+console.log(typeof "");          // string
+console.log(typeof 55);          // number
+console.log(typeof null);        // object
+console.log(typeof undefined);   // undefined
