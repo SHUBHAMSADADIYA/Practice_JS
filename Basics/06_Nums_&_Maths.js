e3e90ad731c7e93852769b83f098fec6d3d2dccc
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-12-01 17:09:42 +0530

    06_Nums_&_Maths.js

---
+++ Basics/06_Nums_&_Maths.js
@@ -0,0 +1,26 @@
+                    // ===========  NUMS ============  // 
+
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)     // It specifies the value has Number Datatype
+// console.log(balance);               // [Number: 100]
+
+// console.log(balance.toString());    // 100
+// // It converts number to String datatype
+
+// console.log(balance.toFixed(2));    // 100.00
+// // It Fix the value only 2 number are allowed after point
+
+// const extraBalance = 154.511516
+// console.log(extraBalance.toPrecision(3));   // 155
+// // It returns the value in Round Figure as the parameter passed
+// // NOTE ===> You want to make sure the parameter value is not lower than the characters before point
+
+// const money = 1000000
+// console.log(money.toLocaleString());        // 10,00,000
+// console.log(money.toLocaleString('en-US')); // 1,000,000 
+
+
+                    // ===========  MATHS ============  // 
+
