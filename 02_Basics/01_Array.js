5477dc9aff05f3b9d46031c71e28c605f1c8dfc9
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-12-02 17:20:38 +0530

    01_Array

--- 02_Basics/01_Array.js
+++ 02_Basics/01_Array.js
@@ -1,53 +1,53 @@
-// // You can Write array in different types
+// You can Write array in different types
 
-// // 1. It includes Numbers in array
-// const arrNo = [0, 1, 2, 3, 4, 5, 6 ]
+// 1. It includes Numbers in array
+const arrNo = [0, 1, 2, 3, 4, 5, 6 ]
 
-// // 2. It includes Names in array
-// const arrHeroName = [ "Hanuman", "Shaktiman", "Shiva"]
+// 2. It includes Names in array
+const arrHeroName = [ "Hanuman", "Shaktiman", "Shiva"]
 
-// // 3. It includes Boolean value in array
-// const arrBoolean = [true]
+// 3. It includes Boolean value in array
+const arrBoolean = [true]
 
-// // 4. It includes Both Numbers, Names, and Boolean in array
-// const arrBoth = [1, "Hanuman", 2, "Shaktiman"]
+// 4. It includes Both Numbers, Names, and Boolean in array
+const arrBoth = [1, "Hanuman", 2, "Shaktiman"]
 
 // // Array Methods
 
 const myArr = [0, 1, 2, 3, 4, 5]
-// console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
+console.log(myArr); // [ 0, 1, 2, 3, 4, 5 ]
 
-// // NOTE ===> It must be Written in given format
-// myArr.push(6)
-// console.log(myArr); // [0, 1, 2, 3, 4, 5, 6]
-// // It add the Given Parameter
+// NOTE ===> It must be Written in given format
+myArr.push(6)
+console.log(myArr); // [0, 1, 2, 3, 4, 5, 6]
+// It add the Given Parameter
 
-// // NOTE ===> It must be Written in given format
-// myArr.pop()
-// console.log(myArr); // [0, 1, 2, 3, 4, 5]
-// // It Removes the last element 
+// NOTE ===> It must be Written in given format
+myArr.pop()
+console.log(myArr); // [0, 1, 2, 3, 4, 5]
+// It Removes the last element 
 
-// myArr.unshift(7)
-// console.log(myArr); // [7, 0, 1, 2, 3, 4, 5]
-// // It adds value at starting of array, Which is not so useful for Big Arrays
+myArr.unshift(7)
+console.log(myArr); // [7, 0, 1, 2, 3, 4, 5]
+// It adds value at starting of array, Which is not so useful for Big Arrays
 
-// myArr.shift()
-// console.log(myArr); // [0, 1, 2, 3, 4, 5]
-// // It removes element from Starting of array
+myArr.shift()
+console.log(myArr); // [0, 1, 2, 3, 4, 5]
+// It removes element from Starting of array
 
-// console.log(myArr.includes(9)); // False
-// console.log(myArr.includes(5)); // True
-// // It Checks the value is in array or not in array
+console.log(myArr.includes(9)); // False
+console.log(myArr.includes(5)); // True
+// It Checks the value is in array or not in array
 
-// console.log(myArr.indexOf(9));  // -1 // it gives '-1' on unavailable value of array
-// console.log(myArr.indexOf(3));  // 3
-// // It checkes the given element is on which index
+console.log(myArr.indexOf(9));  // -1 // it gives '-1' on unavailable value of array
+console.log(myArr.indexOf(3));  // 3
+// It checkes the given element is on which index
 
-// const newArr = myArr.join()
-// console.log(myArr);         // [ 0, 1, 2, 3, 4, 5 ]
-// console.log(newArr);        // 0,1,2,3,4,5
-// console.log(typeof newArr); // string
-// // Join Function converts array into string
+const newArr = myArr.join()
+console.log(myArr);         // [ 0, 1, 2, 3, 4, 5 ]
+console.log(newArr);        // 0,1,2,3,4,5
+console.log(typeof newArr); // string
+// Join Function converts array into string
 
  console.log("A", myArr);   // A [ 0, 1, 2, 3, 4, 5 ]
 
