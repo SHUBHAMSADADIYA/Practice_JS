9cd247e15dfda8ff3e469af3bdbb65aa2149a3be
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-12-26 17:05:12 +0530

    loop

--- 05_Itresions/Two.js
+++ 05_Itresions/Two.js
@@ -1,23 +1,21 @@
 
-// // While Loop
+// While Loop
+i = 0;
+while (i <= 10) {
+    console.log(`Value of i is ${i}`);
+    i = i + 2
+}
 
-// i = 0;
-// while (i <= 10) {
-//     console.log(`Value of i is ${i}`);
-//     i = i + 2
-// }
+// While loop from Array
+let myArray = ["Shubham", "Bhargav", "Prince", "Rohit", "Mihir"]
+let arr = 0;
 
-// // While loop from Array
-// let myArray = ["Shubham", "Bhargav", "Prince", "Rohit", "Mihir"]
-// let arr = 0;
-
-// while (arr < myArray.length) {
-//     console.log(`Name is ${myArray[arr]}`);
-//     arr++; 
-// }
+while (arr < myArray.length) {
+    console.log(`Name is ${myArray[arr]}`);
+    arr++; 
+}
 
 // Do While Loop
-
 score = 0
 do {
     console.log(`Score is ${score}`);
