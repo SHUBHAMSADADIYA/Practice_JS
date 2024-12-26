8e6f67dce2fddc8abb9962eb1396a37770776478
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2024-12-26 17:36:36 +0530

    three

---
+++ 05_Itresions/Three.js
@@ -0,0 +1,40 @@
+
+// For of Loop
+
+// ["", "", ""] // You can make Arrays and Objects like this
+// [{}, {}, {}]
+
+const arr = ["Shubham", 2, "Bhargav", 4, "Prince"]
+for (const num of arr)  // Here num is like variable which contains  value of looping variable
+{
+    console.log(num);
+}
+
+const greeting = "Hello Welcome!!!"
+console.log(`Each Alfabate of ${greeting} is :`);
+for (const greet of greeting) {
+    console.log(greet);
+}
+
+// Maps // It is known for his 'entry of Unique value only','Display in same order'
+
+const map = new Map()
+map.set('IND',"INDIA")
+map.set('USA',"United States of America")
+map.set('FRC',"France")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ' ==> ', value);
+}
+
+// const myObject = {
+//     'game1' : 'GTA V',
+//     'game2' : 'Forza Horizen',
+//     'game3' : 'Mortal Kombat'
+// }
+
+// for (const [key ,value] of myObject) {
+//     console.log(key, ' ==> ', value);
+// }
