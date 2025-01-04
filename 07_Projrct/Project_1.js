9b5449125efca2223fef4fdd02508b9f75181f9a
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2025-01-04 16:36:17 +0530

    Project_1

--- 07_Projrct/Project_1.js
+++ 07_Projrct/Project_1.js
@@ -1,3 +1,24 @@
-const button = document.querySelectorAll('bt')
-const body = document.querySelector("body")
 
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'red') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blueviolet') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+    
+  });
+});
