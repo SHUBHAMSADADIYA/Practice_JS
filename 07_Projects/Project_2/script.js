07f799032652489e99144b3c98e7e3b0ceaee6c4
SHUBHAMSADADIYA <SADADIYASHUBHAM5@GMAIL.com>
2025-01-05 11:02:08 +0530

    Project 2

--- 07_Projects/Project_2/script.js
+++ 07_Projects/Project_2/script.js
@@ -7,8 +7,8 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     const height = parseInt(document.querySelector('#height').value)
-    const Weight = parseInt(document.querySelector('#Weight').value)
-    const result = document.querySelector('#result')
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
 
     if(height === '' || height < 0 || isNaN(height)) {
         result.innerHTML = `Please Enter a Valid Height ${height}`;
@@ -17,8 +17,8 @@ form.addEventListener('submit', function(e) {
         result.innerHTML = `Please Enter a Valid Height ${height}`;
     }
     else {
-        const bmi = (weight / ((height*height)/10000).toFixed(2))
+        const bmi = (weight / ((height*height)/10000)).toFixed(2)
         // Show the Result
-        result.innerHTML = `<span>${bmi}</span>`
+        result.innerHTML = `BMI is : <span>${bmi}</span>`
     }
 })
